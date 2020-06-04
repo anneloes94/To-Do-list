a7ff02a1a99a3e64b7aad5f011d05d0021d0cf63
Anneloes <anneloes9405@hotmail.com>
2020-06-04 18:15:33 -0400

    listitems are pushed to list
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+let items = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
@@ -23,10 +25,20 @@ app.get("/", function(req, res) {
   
   let day = today.toLocaleDateString("en-US", options)
 
-  res.render('list', {day: day})
+  console.log(typeof(items))
+  // if (items.length > 0) {
+  //   items.forEach(function(item) {
+  //     console.log(item)
+  //   })
+  // }
+
+  res.render('list', {day: day, newItems: items})
 })
 
-app.post("/", )
+app.post("/", function(req, res) {
+  items.push(req.body.newItem)
+  res.redirect("/")
+})
 
 app.listen(port, function(){
   console.log(`Server started on port ${port}`)
